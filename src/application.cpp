7f1a2a3ab6802129cf8ca5dca3614239a035f047
vellaggio <mikl.tm_7@hotmail.com>
2014-05-12 11:51:23 +0200

    first commit!

---
+++ src/application.cpp
@@ -0,0 +1,93 @@
+#include "application.h"
+#include "utils.h"
+#include "image.h"
+
+Application::Application(const char* caption, int width, int height)
+{
+	this->window = createWindow(caption, width, height);
+
+	// initialize attributes
+	// Warning: DO NOT CREATE STUFF HERE, USE THE INIT 
+	// things create here cannot access opengl
+	int w,h;
+	SDL_GetWindowSize(window,&w,&h);
+
+	this->window_width = w;
+	this->window_height = h;
+	this->keystate = SDL_GetKeyboardState(NULL);
+}
+
+//Here we have already GL working, so we can create meshes and textures
+void Application::init(void)
+{
+	std::cout << "initiating app..." << std::endl;
+
+	//here add your init stuff
+}
+
+//render one frame
+void Application::render(void)
+{
+	// Clear the window and the depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//render grid
+	Image img( 400, 300);
+	for(unsigned int x = 0; x < img.width; x++)
+		for(unsigned int y = 0; y < img.height; y++)
+		{
+			if( x % 100 == 0 || y % 100 == 0 )
+				img.setPixel(x,y, Color::WHITE );
+			else if( x % 25 == 0 || y % 25 == 0 )
+				img.setPixel(x,y, Color::GRAY);
+		}
+
+	img.scale( this->window_width, this->window_height );
+
+	renderImage( &img );
+
+	//swap between front buffer and back buffer
+	SDL_GL_SwapWindow(this->window);
+}
+
+//called after render
+void Application::update(double seconds_elapsed)
+{
+	if (keystate[SDL_SCANCODE_SPACE])
+	{
+		//...
+	}
+}
+
+//keyboard press event 
+void Application::onKeyPressed( SDL_KeyboardEvent event )
+{
+	switch(event.keysym.sym)
+	{
+		case SDLK_ESCAPE: exit(0); break; //ESC key, kill the app
+	}
+}
+
+//mouse button event
+void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse pressed
+	{
+
+	}
+}
+
+void Application::onMouseButtonUp( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse unpressed
+	{
+
+	}
+}
+
+//when the app starts
+void Application::start()
+{
+	std::cout << "launching loop..." << std::endl;
+	launchLoop(this);
+}
